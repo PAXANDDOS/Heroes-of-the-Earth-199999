a0a75440d163ff835aae2870a5815dad58051105
Paul <40315177+PAXANDDOS@users.noreply.github.com>
2021-04-16 13:21:34 +0300

    v0.8 update

---
+++ application/views/404_view.php
@@ -0,0 +1,8 @@
+<style>@import url('../../assets/css/404.css');</style>
+<div class="body unselectable">
+    <h1>404</h1>
+    <span>Not found &#9785;</span>
+    <form method="post">
+        <input type="submit" name="back" value="Go back">
+    </form>
+</div>
