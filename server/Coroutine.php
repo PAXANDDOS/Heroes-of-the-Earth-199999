0b4d7e3fb02b47c21fe003d62fa3712b1cca4bde
Lenartov <pirynex@gmail.com>
2021-04-19 01:46:47 +0300

    deck update
    Я не могу захостить нормально сайт, что бы проверить, работают ли мои изменения, так что нужно обязательно  проверить

--- server/Coroutine.php
+++ server/Coroutine.php
@@ -165,29 +165,3 @@ function getTaskId()
         $scheduler->schedule($task);
     });
 }
-
-/*function childTask() 
-{
-    $tid = (yield getTaskId());
-    while (true) 
-    {
-        echo "Child task $tid still alive!\n";
-        yield;
-    }
-}
-
-function task() {
-    $tid = (yield getTaskId());
-    $childTid = (yield newTask(childTask()));
-
-    for ($i = 1; $i <= 6; ++$i) {
-        echo "Parent task $tid iteration $i.\n";
-        yield;
-
-        if ($i == 3) yield killTask($childTid);
-    }
-}
-
-$scheduler = new Scheduler;
-$scheduler->newTask(task());
-$scheduler->run();*/
