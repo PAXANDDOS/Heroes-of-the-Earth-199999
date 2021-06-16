caa69b5ba572c044ce22d4323e6cf5d65914ef86
Paul <40315177+PAXANDDOS@users.noreply.github.com>
2021-06-16 15:13:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
-include_once "SocketServer.php";
-include_once "GameSession.php";
-include_once "Coroutine.php";
+require_once "SocketServer.php";
+require_once "GameSession.php";
+require_once "Coroutine.php";
 
 set_time_limit(0);
 $sock = new SocketServer('localhost', 80, 20);
-
 
 $sock->listen();
 
 $scheduler = new Scheduler;
 $scheduler->newTask($sock->connectClients());
-
 
 $scheduler->run();
 
