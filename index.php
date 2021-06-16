2caef0445f6371e43d2bf6d9e8c7d3f1e29a0b80
Paul <40315177+PAXANDDOS@users.noreply.github.com>
2021-06-16 15:03:11 +0300

    Server

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
-    session_start();
-    ini_set('display_errors', 1);
-    require_once 'application/bootstrap.php';
-?>
+include_once "SocketServer.php";
+include_once "GameSession.php";
+include_once "Coroutine.php";
+
+set_time_limit(0);
+$sock = new SocketServer('0.0.0.0', 80, 20);
+
+
+$sock->listen();
+
+$scheduler = new Scheduler;
+$scheduler->newTask($sock->connectClients());
+
+
+$scheduler->run();
+
